e87b50a67713ffdd70fd9b4708f985094b9e2ea2
s1f10220187 <107394091+1f10220187@users.noreply.github.com>
2025-02-23 00:31:46 +0900

    Googleアカウントを用いたログイン機能

--- chat-train/src/components/SignIn.js
+++ chat-train/src/components/SignIn.js
@@ -1,9 +1,12 @@
 import { Button } from '@mui/material'
 import React from 'react'
+import firebase from "firebase/compat/app"
+import {auth} from "../firebase.js"
 
 function SignIn() {
     function signInWithGoogle(){
-        
+        const probider = new firebase.auth.GoogleAuthProvider();
+        auth.signInWithPopup(probider);
     }
   return (
     <div>
